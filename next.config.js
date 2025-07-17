56568d277c31017a50aa89de1b2067a54126d8dc
LaurierChenier <laurierllp@gmail.com>
2025-07-17 08:42:58 -0400

    fix: minimal Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-        ],
-      },
-    ]
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
